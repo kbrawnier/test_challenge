e2816fe6e510ec3ad9e8b39dc3ba9bed92907ac7
kbrawnier <38894973+kbrawnier@users.noreply.github.com>
2021-09-23 23:00:37 -0500

    adding footer and javascript

--- testWebsite/arrayFile.js
+++ testWebsite/arrayFile.js
@@ -1,12 +1,17 @@
 var isClicked = false;
 var arrayOutput = document.getElementById('arrayOutput');
-
-arrayOutput.clicked = function(){
-    isClicked = true;
-}
+var modal = document.getElementById('modal');
+const array1 = ["Matt Johnson", "Bart Paden", "Ryan Doss", "Jared Malcolm"];
+const array2 = ["Matt Johnson", "Bart Paden", "Jordan Heigle", "Tyler Viles"];
 
 function arrayFunction(){
-    if(isClicked == true){
-        arrayOutput.textContent()
+    console.log('clicked');
+    if(isClicked == false){
+    
+        isClicked = true;
+    } else{document.getElementById('modal').style.display = 'flex'} 
+    };
+
+    function arrayOutput(array1,array2){
+        const newArray = array1.concat(array2)
     }
-}
